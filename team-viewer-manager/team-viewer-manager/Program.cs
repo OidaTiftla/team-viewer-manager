e40e3078dbb9bb4bdb240a69fece140e0cb54f91
STC <stc@spangler-automation.de>
2019-09-23 18:32:23 +0200

    login

--- team-viewer-manager/team-viewer-manager/Program.cs
+++ team-viewer-manager/team-viewer-manager/Program.cs
@@ -1,9 +1,59 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using team_viewer_manager.TeamViewer;
 
 namespace team_viewer_manager {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+    internal class Program {
+
+        private static async Task Main(string[] args) {
+            if (!tryGetAuthorizationToken(out var token)) {
+                Console.Write("Please type the authorization token: ");
+                token = Console.ReadLine().Trim();
+            }
+
+            try {
+                var tvClient = new TeamViewerApiClient();
+                if (!await tvClient.Authorize(token)) {
+                    throw new Exception("Login failed");
+                }
+                ConsoleWriteLineSuccess("Login successfully");
+            } catch (Exception ex) {
+                ConsoleWriteLineError("Exception occured:");
+                ConsoleWriteLineError(ex.ToString());
+            }
         }
+
+        private static bool tryGetAuthorizationToken(out string token) {
+            if (File.Exists("authorization.token")) {
+                token = File.ReadAllText("authorization.token");
+                return !string.IsNullOrWhiteSpace(token);
+            }
+            token = null;
+            return false;
+        }
+
+        #region console helpers
+
+        private static void ConsoleWriteLineSuccess(string text) {
+            ConsoleWriteLineColor(text, ConsoleColor.DarkGreen);
+        }
+
+        private static void ConsoleWriteLineError(string text) {
+            ConsoleWriteLineColor(text, ConsoleColor.Red);
+        }
+
+        private static void ConsoleWriteLineColor(string text, ConsoleColor color) {
+            var oldColor = Console.ForegroundColor;
+            try {
+                Console.ForegroundColor = color;
+                Console.WriteLine(text);
+            } finally {
+                Console.ForegroundColor = oldColor;
+            }
+        }
+
+        #endregion console helpers
     }
 }
---
+++ team-viewer-manager/team-viewer-manager/TeamViewer/TeamViewerApiClient.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace team_viewer_manager.TeamViewer {
+
+    public class TeamViewerApiClient {
+
+        #region properties
+
+        private readonly HttpClient client_;
+
+        #endregion properties
+
+        #region constructors and destructor
+
+        public TeamViewerApiClient()
+            : this("https://webapi.teamviewer.com/") { }
+
+        public TeamViewerApiClient(string baseUrl)
+            : this(new Uri(baseUrl)) { }
+
+        public TeamViewerApiClient(Uri baseUrl) {
+            this.client_ = new HttpClient();
+            this.client_.BaseAddress = baseUrl;
+            this.client_.DefaultRequestHeaders.Accept.Clear();
+            this.client_.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        #endregion constructors and destructor
+
+        // api/v1/devices
+
+        #region Authorization
+
+        public async Task<bool> Authorize(string token) {
+            this.client_.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await this.client_.GetAsync("api/v1/ping");
+            if (response.StatusCode != System.Net.HttpStatusCode.OK) {
+                throw new Exception($"Login failed with status code {response.StatusCode} ({(int)response.StatusCode})");
+            }
+            var responseJson = await response.Content.ReadAsStringAsync();
+            dynamic responseJsonObject = JsonConvert.DeserializeObject(responseJson);
+            return (bool)responseJsonObject.token_valid == true;
+        }
+
+        #endregion Authorization
+    }
+}
