6e47e8335f14023ea2824d29cd6e03a76582a1a5
STC <stc@spangler-automation.de>
2019-09-23 17:50:52 +0200

    create project

---
+++ team-viewer-manager/team-viewer-manager/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace team_viewer_manager {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
